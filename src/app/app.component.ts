3b77af4b1ff47ae02aecd5579bff52d1f9d0f4f2
kev00kev <c14180089@john.petra.ac.id>
2021-02-23 20:51:17 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,6 +27,9 @@ EventClick(){
       }
       else
       {
+        this.a = this.a - 1;
+        this.b = this.b - 1;
+
             if (this.counter % 2 != 0){
               this.user = 1;
             }
@@ -38,6 +41,60 @@ EventClick(){
             this.counter = this.counter + 1;
       }
   }
+if(
+   this.board[0][0] == "0" &&
+   this.board[0][1] == "0" &&
+   this.board[0][2] == "0" &&
+   this.board[0][3] == "0" ||
+
+   this.board[1][0] == "0" &&
+   this.board[1][1] == "0" &&
+   this.board[1][2] == "0" &&
+   this.board[1][3] == "0" ||
+
+   this.board[2][0] == "0" &&
+   this.board[2][1] == "0" &&
+   this.board[2][2] == "0" &&
+   this.board[2][3] == "0" ||
+
+   this.board[3][0] == "0" &&
+   this.board[3][1] == "0" &&
+   this.board[3][2] == "0" &&
+   this.board[3][3] == "0" ||
+
+   this.board[4][0] == "0" &&
+   this.board[4][1] == "0" &&
+   this.board[4][2] == "0" &&
+   this.board[4][3] == "0" ||
+
+   this.board[0][1] == "0" &&
+   this.board[0][2] == "0" &&
+   this.board[0][3] == "0" &&
+   this.board[0][4] == "0" ||
+
+   this.board[1][1] == "0" &&
+   this.board[1][2] == "0" &&
+   this.board[1][3] == "0" &&
+   this.board[1][4] == "0" ||
+
+   this.board[2][1] == "0" &&
+   this.board[2][2] == "0" &&
+   this.board[2][3] == "0" &&
+   this.board[2][4] == "0" ||
+
+   this.board[3][1] == "0" &&
+   this.board[3][2] == "0" &&
+   this.board[3][3] == "0" &&
+   this.board[3][4] == "0" ||
+
+   this.board[4][1] == "0" &&
+   this.board[4][2] == "0" &&
+   this.board[4][3] == "0" &&
+   this.board[4][4] == "0" ||
+  ){
+
+  }
+  
 
 }
 
