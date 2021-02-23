275947d26492e6f1a33261fd551ea4d614175f76
kev00kev <c14180089@john.petra.ac.id>
2021-02-23 20:04:34 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,11 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  user = 0;
   index = 0;
+  a = 0;
+  b = 0;
+  counter = 1;
   board: string[][] = [
      ["*", "*", "*", "*", "*"],
      ["*", "*", "*", "*", "*"],
@@ -15,4 +19,26 @@ export class AppComponent  {
      ["*", "*", "*", "*", "*"],
      ["*", "*", "*", "*", "*"]
 ];
+EventClick(){
+  
+  if (this.a > 0 && this.a < 6 && this.b > 0 && this.b < 6){
+      if(this.board[this.a-1][this.b-1] == "*"){
+          alert('Isi yang lain !!!')
+      }
+      else
+      {
+            if (this.counter % 2 != 0){
+              this.user = 1;
+            }
+            else
+            {
+              this.user = 0;
+
+            }
+            this.counter = this.counter + 1;
+      }
+  }
+
+}
+
 }
